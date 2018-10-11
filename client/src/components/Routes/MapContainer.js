f70c4133d6ed28a658e9bf74cfe2b423d265722b
Mapno <mariorvinas@gmail.com>
2018-10-11 21:24:42 +0200

    creates route

--- client/src/components/Routes/MapContainer.js
+++ client/src/components/Routes/MapContainer.js
@@ -13,21 +13,34 @@ export default class MapContainer extends React.Component {
             center: { lat: 41.0082, lng: 28.9784 },
             zoom: 8
         })
-        this.setState({map});
+        this.setState({ map });
+    }
+
+    onRoute() {
+        const directionsService = new window.google.maps.DirectionsService();
+        const directionsDisplay = new window.google.maps.DirectionsRenderer({ map: this.state.map });
+        const request = {
+            origin: { lat: this.props.departure.lat, lng: this.props.departure.lng },
+            destination: { lat: this.props.arrival.lat, lng: this.props.arrival.lng },
+            travelMode: 'DRIVING'
+        };
+        directionsService.route(request, (res,status) => {
+            if(status === 'OK') directionsDisplay.setDirections(res)
+        })
     }
 
     onSelect = () => {
-        const {lat, lng} = this.props.departure
+        const { lat, lng } = this.props.departure
         new window.google.maps.Marker({
             position: { lat: this.props.departure.lat, lng: this.props.departure.lng },
             map: this.state.map,
             title: this.props.departure.addres
         });
-        console.log()
-        this.state.map.setCenter({lat,lng})
+        this.state.map.setCenter({ lat, lng })
     }
     componentDidUpdate() {
-        this.onSelect()
+        console.log(this.props.arrival)
+        this.props.arrival !== {} ? this.onRoute() : this.onSelect()
     }
 
     render() {
