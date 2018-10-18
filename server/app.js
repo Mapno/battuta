ca5d899a50bf70ab4eb07730b47faf515fa421ad
Mapno <mariorvinas@gmail.com>
2018-10-18 13:05:27 +0200

    react html

--- server/app.js
+++ server/app.js
@@ -60,4 +60,9 @@ app.use('/api/auth', authRouter);
 const routeRouter = require('./routes/routes');
 app.use('/api/route', routeRouter);
 
+app.use((req, res, next) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/public/index.html");
+});
+
 module.exports = app;
