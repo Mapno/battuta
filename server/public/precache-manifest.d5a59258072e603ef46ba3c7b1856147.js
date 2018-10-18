037e76595f6f5b7a4bb603ffe87331d56468e486
Mapno <mariorvinas@gmail.com>
2018-10-18 13:11:39 +0200

    build

---
+++ server/public/precache-manifest.d5a59258072e603ef46ba3c7b1856147.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e5cb562e5b26c0d1847",
+    "url": "/static/js/main.2e5cb562.chunk.js"
+  },
+  {
+    "revision": "712eb86a7579b107997f",
+    "url": "/static/js/1.712eb86a.chunk.js"
+  },
+  {
+    "revision": "2e5cb562e5b26c0d1847",
+    "url": "/static/css/main.392967e9.chunk.css"
+  },
+  {
+    "revision": "712eb86a7579b107997f",
+    "url": "/static/css/1.b1626f0e.chunk.css"
+  },
+  {
+    "revision": "bf882147d4aceec12e3161ca6b1252ed",
+    "url": "/index.html"
+  }
+];
